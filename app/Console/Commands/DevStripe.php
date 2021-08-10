c2c47ecdc1f5d17c6ff3550c62c4e6dae609e550
Paul Balan <paulbalan99@gmail.com>
2021-08-10 18:34:19 +0300

    feat: added option to skip stripe init from console

--- app/Console/Commands/DevStripe.php
+++ app/Console/Commands/DevStripe.php
@@ -39,50 +39,52 @@ class DevStripe extends Command
      */
     public function handle()
     {
-        $env = [];
+        if ($this->confirm('Do you wish to setup Stripe now? This can be done later from the dashboard.', false)) {
+            $env = [];  
 
-        $stripeKey = DotenvEditor::getValue('STRIPE_KEY');
-        $stripeSecret = DotenvEditor::getValue('STRIPE_SECRET');
-        $stripeWebhookSecret = DotenvEditor::getValue('STRIPE_WEBHOOK_SECRET');
+            $stripeKey = DotenvEditor::getValue('STRIPE_KEY');
+            $stripeSecret = DotenvEditor::getValue('STRIPE_SECRET');
+            $stripeWebhookSecret = DotenvEditor::getValue('STRIPE_WEBHOOK_SECRET');
 
-        if($stripeKey == '')
-        {
-            $this->info("Stripe public key is missing from the .env file. You can get this value from your Stripe account.");
-            $env['STRIPE_KEY'] = $this->ask('Stripe public key');
+            if($stripeKey == '')
+            {
+                $this->info("Stripe public key is missing from the .env file. You can get this value from your Stripe account.");
+                $env['STRIPE_KEY'] = $this->ask('Stripe public key');
+            }
+
+            if($stripeSecret == '')
+            {
+                $this->info("Stripe secret key is missing from the .env file. You can get this value from your Stripe account.");
+                $env['STRIPE_SECRET'] = $this->ask('Stripe secret key');
+                $stripeSecret = $env['STRIPE_SECRET'];
+            }
+
+            if($stripeWebhookSecret == '')
+            {
+                $this->info("Stripe webhook secret key is missing from the .env file. You can get this value from your Stripe account.");
+                $env['STRIPE_WEBHOOK_SECRET'] = $this->ask('Stripe webhook secret key');
+            }
+
+            if(!empty($env))
+            {
+                DotenvEditor::setKeys($env);
+                DotenvEditor::save();
+            }
+
+            //Create a mock Stripe product and save it to the database
+            $stripe = new \Stripe\StripeClient($stripeSecret);
+            $product = $stripe->products->create([
+                'name' => 'default',
+            ]);
+
+            //Add subscription to the database
+            $subscriptionType = new SubscriptionType();
+            $subscriptionType->name = 'default';
+            $subscriptionType->product_id = $product['id'];
+            $subscriptionType->public = '1';
+            $subscriptionType->save();
+
+            $this->info("Stripe was configured successfully.");
         }
-
-        if($stripeSecret == '')
-        {
-            $this->info("Stripe secret key is missing from the .env file. You can get this value from your Stripe account.");
-            $env['STRIPE_SECRET'] = $this->ask('Stripe secret key');
-            $stripeSecret = $env['STRIPE_SECRET'];
-        }
-
-        if($stripeWebhookSecret == '')
-        {
-            $this->info("Stripe webhook secret key is missing from the .env file. You can get this value from your Stripe account.");
-            $env['STRIPE_WEBHOOK_SECRET'] = $this->ask('Stripe webhook secret key');
-        }
-
-        if(!empty($env))
-        {
-            DotenvEditor::setKeys($env);
-            DotenvEditor::save();
-        }
-
-        //Create a mock Stripe product and save it to the database
-        $stripe = new \Stripe\StripeClient($stripeSecret);
-        $product = $stripe->products->create([
-            'name' => 'default',
-        ]);
-
-        //Add subscription to the database
-        $subscriptionType = new SubscriptionType();
-        $subscriptionType->name = 'default';
-        $subscriptionType->product_id = $product['id'];
-        $subscriptionType->public = '1';
-        $subscriptionType->save();
-
-        $this->info("Stripe was configured successfully.");
     }
 }
